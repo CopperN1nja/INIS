66a82554d445b9b1cf905e5d9eeec2b023c79cd8
CopperN1nja <senkevich.vlad234@gmail.com>
2022-05-06 01:22:32 +0300

    finish make details1

--- main.js
+++ main.js
@@ -2,22 +2,52 @@
 //       You can access this variable here, and should use this variable here to build your webpages
 
 let initProducts = () => {
-    // To see the shirts object, run:
-    // console.log(shirts);
-    document.getElementById('image').src = shirts[0].colors.blue.front;
-    document.getElementById('image2').src = shirts[1].colors.blue.front;
-    document.getElementById('image3').src = shirts[2].colors.white.back;
-    document.getElementById('image4').src = shirts[3].colors.white.front;
-    document.getElementById('name').innerHTML = shirts[0].name;
-    document.getElementById('name2').innerHTML = shirts[1].name;
-    document.getElementById('name3').innerHTML = shirts[2].name;
-    document.getElementById('name4').innerHTML = shirts[3].name;
-    // Your Code Here
+  // To see the shirts object, run:
+  // console.log(shirts);
+  // Your Code Here
+  document.getElementById('img1').src = shirts[0].colors.white.front;
+  document.getElementById('name1').innerHTML = shirts[0].name;
+
+  document.getElementById('img2').src = shirts[1].colors.white.front;
+  document.getElementById('name2').innerHTML = shirts[1].name;
+
+  document.getElementById('img3').src = shirts[2].colors.white.front;
+  document.getElementById('name3').innerHTML = shirts[2].name;
+
+  document.getElementById('img4').src = shirts[3].colors.white.front;
+  document.getElementById('name4').innerHTML = shirts[3].name;
+  
 };
 
-let initDetails = () => {
-    // To see the shirts object, run:
-    // console.log(shirts);
+const name1 = document.createElement('h2');
+name1.className = "name";
+name1.innerHTML = shirts[0].name;
+const image1 = document.createElement('img')
+const cost = document.createElement('h2')
+cost.className = 'cost'
+cost.innerHTML = shirts[0].price;
+image1.src = shirts[0].colors.white.front;
+document.querySelector('.details').appendChild(image1);
+document.querySelector('.details').appendChild(cost);
+document.querySelector('.details').appendChild(name1);
+n=0;
+Change(n);
 
-    // Your Code Here
-};
+function Change(n){
+    if(n==1){
+        image1.src = shirts[0].colors.white.front;
+    }
+    if(n==2){
+        image1.src = shirts[0].colors.blue.front;
+    }
+    if(n==3){
+        image1.src = shirts[0].colors.red.front;
+    }
+    if(n==4){
+        image1.src = shirts[0].colors.pink.front;
+    }
+    if(n==5){
+      image1.src = shirts[0].colors.white.back;
+  }
+}
+
